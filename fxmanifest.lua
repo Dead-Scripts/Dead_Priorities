ed24d06b3a528b29c884a3f54f08fe4cfeae45d7
Dead-Scripts <btiede590@gmail.com>
2025-08-25 17:29:37 -0500

    update

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+author "Dead Scripts"
+description "Dead Priorites"
+fx_version "cerulean"
+game "gta5"
+
+client_script "client.lua"
+
+server_script "server.lua"
+
+shared_scripts {
+    "config.lua",
+}
